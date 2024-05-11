def8a3320d0a370e0d2ad37c57d0465d9781a85f
ewertonilima <ewertoninaciolima@gmail.com>
2024-05-11 19:43:32 -0300

    init: :tada: Hello world lambda

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,30 @@
+package helloworld;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            return response.withStatusCode(200).withBody("{\"message\": \"hello world\" }");
+        } catch (Exception e) {
+            return response.withStatusCode(500)
+                    .withBody("{\"message\": \"hello world is not working\" }");
+        }
+    }
+}
